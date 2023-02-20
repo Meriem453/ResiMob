1e68c5d2050e6efa45b5678dad24747c098ff5bd
MezSpire <mimi.04.05.2003@gmail.com>
2023-02-20 21:52:40 +0100

    add user activity first design

---
+++ app/src/main/java/com/example/residence_app/AddUserActivity.kt
@@ -0,0 +1,11 @@
+package com.example.residence_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_user)
+    }
+}
--- app/src/main/java/com/example/residence_app/LoginActivity.kt
+++ app/src/main/java/com/example/residence_app/LoginActivity.kt
@@ -1,11 +1,30 @@
 package com.example.residence_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintSet.Layout
+import androidx.core.view.ViewCompat
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val eclipse=findViewById<ImageView>(R.id.eclipse)
+        val card =findViewById<CardView>(R.id.cardView)
+        val login=findViewById<Button>(R.id.btn_login)
+       eclipse.bringToFront()
+        eclipse.invalidate()
+        card.invalidate()
+
+        login.setOnClickListener {
+            val intent=Intent(this,AddUserActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
