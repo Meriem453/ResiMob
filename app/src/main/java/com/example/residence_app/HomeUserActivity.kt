5e528743715262312c070ac227782fcc62ab038b
MezSpire <mimi.04.05.2003@gmail.com>
2023-02-23 02:08:04 +0100

    still adding some design details for user home activity

---
+++ app/src/main/java/com/example/residence_app/HomeUserActivity.kt
@@ -0,0 +1,11 @@
+package com.example.residence_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_user)
+    }
+}
