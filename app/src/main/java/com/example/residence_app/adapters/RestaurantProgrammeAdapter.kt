26d495ca8d7b5be6b8628409491d30153707f978
Abdelhai1 <m_guir@estin.dz>
2023-03-02 08:22:11 +0100

    programme back
    ;

--- app/src/main/java/com/example/residence_app/adapters/RestaurantProgrammeAdapter.kt
+++ app/src/main/java/com/example/residence_app/adapters/RestaurantProgrammeAdapter.kt
@@ -8,9 +8,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.residence_app.R
 import com.example.residence_app.data.RestaurantProgrammeCardData
+import com.example.residence_app.data.ResteurantTimingCardData
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RestaurantProgrammeAdapter(var c:Context): RecyclerView.Adapter<RestaurantProgrammeAdapter.programmeVH>() {
     private var data=ArrayList<RestaurantProgrammeCardData>()
+    private lateinit var db : FirebaseFirestore
 
     inner class programmeVH(itemView: View) : RecyclerView.ViewHolder(itemView){
         val day = itemView.findViewById<TextView>(R.id.restaurant_programme_card_day)
@@ -41,14 +45,83 @@ class RestaurantProgrammeAdapter(var c:Context): RecyclerView.Adapter<Restaurant
     }
 
     fun getData(){
-        //here you implement the data from firebase and you fill the array list data
-       data.add(RestaurantProgrammeCardData("Sunday","Lunch","Dinner","Couscous","Spaghetti"))
-        data.add(RestaurantProgrammeCardData("Monday","Lunch","Dinner","Couscous","Spaghetti"))
-        data.add(RestaurantProgrammeCardData("Tuesday","Lunch","Dinner","Couscous","Spaghetti"))
-        data.add(RestaurantProgrammeCardData("Wednesday","Lunch","Dinner","Couscous","Spaghetti"))
-        data.add(RestaurantProgrammeCardData("Thursday","Lunch","Dinner","Couscous","Spaghetti"))
-        data.add(RestaurantProgrammeCardData("Friday","Lunch","Dinner","Couscous","Spaghetti"))
-        data.add(RestaurantProgrammeCardData("Saturday","Lunch","Dinner","Couscous","Spaghetti"))
-        notifyDataSetChanged()
+        db = FirebaseFirestore.getInstance()
+        var d1 : DocumentReference = db.collection("programme restau").document("saturday")
+        var d2 : DocumentReference = db.collection("programme restau").document("sunday")
+        var d3 : DocumentReference = db.collection("programme restau").document("monday")
+        var d4 : DocumentReference = db.collection("programme restau").document("thuesday")
+        var d5: DocumentReference = db.collection("programme restau").document("wednesday")
+        var d6 : DocumentReference = db.collection("programme restau").document("thursday")
+        var d7 : DocumentReference = db.collection("programme restau").document("friday")
+
+
+        d1.get().addOnCompleteListener() {
+            var day = it.result!!.data?.getValue("day").toString().trim()
+            var label1 = it.result!!.data?.getValue("label1").toString().trim()
+            var label2 = it.result!!.data?.getValue("label2").toString().trim()
+            var meal1 = it.result!!.data?.getValue("meal1").toString().trim()
+            var meal2 = it.result!!.data?.getValue("meal2").toString().trim()
+            data.add(RestaurantProgrammeCardData(day,label1,label2,meal1,meal2))
+            notifyDataSetChanged()
+
+            d2.get().addOnCompleteListener() {
+                var day = it.result!!.data?.getValue("day").toString().trim()
+                var label1 = it.result!!.data?.getValue("label1").toString().trim()
+                var label2 = it.result!!.data?.getValue("label2").toString().trim()
+                var meal1 = it.result!!.data?.getValue("meal1").toString().trim()
+                var meal2 = it.result!!.data?.getValue("meal2").toString().trim()
+                data.add(RestaurantProgrammeCardData(day, label1, label2, meal1, meal2))
+                notifyDataSetChanged()
+                d3.get().addOnCompleteListener() {
+                    var day = it.result!!.data?.getValue("day").toString().trim()
+                    var label1 = it.result!!.data?.getValue("label1").toString().trim()
+                    var label2 = it.result!!.data?.getValue("label2").toString().trim()
+                    var meal1 = it.result!!.data?.getValue("meal1").toString().trim()
+                    var meal2 = it.result!!.data?.getValue("meal2").toString().trim()
+                    data.add(RestaurantProgrammeCardData(day, label1, label2, meal1, meal2))
+                    notifyDataSetChanged()
+                    d4.get().addOnCompleteListener() {
+                        var day = it.result!!.data?.getValue("day").toString().trim()
+                        var label1 = it.result!!.data?.getValue("label1").toString().trim()
+                        var label2 = it.result!!.data?.getValue("label2").toString().trim()
+                        var meal1 = it.result!!.data?.getValue("meal1").toString().trim()
+                        var meal2 = it.result!!.data?.getValue("meal2").toString().trim()
+                        data.add(RestaurantProgrammeCardData(day, label1, label2, meal1, meal2))
+                        notifyDataSetChanged()
+                        d5.get().addOnCompleteListener() {
+                            var day = it.result!!.data?.getValue("day").toString().trim()
+                            var label1 = it.result!!.data?.getValue("label1").toString().trim()
+                            var label2 = it.result!!.data?.getValue("label2").toString().trim()
+                            var meal1 = it.result!!.data?.getValue("meal1").toString().trim()
+                            var meal2 = it.result!!.data?.getValue("meal2").toString().trim()
+                            data.add(RestaurantProgrammeCardData(day, label1, label2, meal1, meal2))
+                            notifyDataSetChanged()
+                            d6.get().addOnCompleteListener() {
+                                var day = it.result!!.data?.getValue("day").toString().trim()
+                                var label1 = it.result!!.data?.getValue("label1").toString().trim()
+                                var label2 = it.result!!.data?.getValue("label2").toString().trim()
+                                var meal1 = it.result!!.data?.getValue("meal1").toString().trim()
+                                var meal2 = it.result!!.data?.getValue("meal2").toString().trim()
+                                data.add(RestaurantProgrammeCardData(day, label1, label2, meal1, meal2))
+                                notifyDataSetChanged()
+                                d7.get().addOnCompleteListener() {
+                                    var day = it.result!!.data?.getValue("day").toString().trim()
+                                    var label1 = it.result!!.data?.getValue("label1").toString().trim()
+                                    var label2 = it.result!!.data?.getValue("label2").toString().trim()
+                                    var meal1 = it.result!!.data?.getValue("meal1").toString().trim()
+                                    var meal2 = it.result!!.data?.getValue("meal2").toString().trim()
+                                    data.add(RestaurantProgrammeCardData(day, label1, label2, meal1, meal2))
+                                    notifyDataSetChanged()
+                                }
+                            }
+                        }
+                    }
+                            }
+            }
+
+
+            }
+
+
     }
 }
