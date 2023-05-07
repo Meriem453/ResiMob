7c22338f2a14183da969115a73b925007f0f0f78
yacineboudebouz <104728737+yacineboudebouz@users.noreply.github.com>
2023-05-07 01:33:39 +0100

    nav drawer

--- app/src/main/java/com/example/residence_app/MainActivity.kt
+++ app/src/main/java/com/example/residence_app/MainActivity.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
@@ -114,6 +115,14 @@ class MainActivity : BaseActivity() {
                     }
                     R.id.feedback_screen -> {
                         startActivity(Intent(baseContext,FeedbackActivity::class.java))
+                    }
+                    R.id.user_language -> {
+                        ChangeLanguage().show(supportFragmentManager,"ll")
+                        true
+                    }
+                    R.id.user_privacy -> {
+                        startActivity(Intent(baseContext,PrivacyPolicy::class.java))
+                        true
                     }
                     R.id.LogOut-> {
                         LogoutUser(this).show(supportFragmentManager,"logout")
